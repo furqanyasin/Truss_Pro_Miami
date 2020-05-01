badcc71dd1332ea7cf81c9b03df4c45c4da79195
furqanyasin <furqanyasin1997@gmail.com>
2020-05-01 17:50:09 +0500

    add slider banner

--- app/src/main/java/com/example/trusspromiami/MainActivity.java
+++ app/src/main/java/com/example/trusspromiami/MainActivity.java
@@ -6,15 +6,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.viewpager.widget.ViewPager;
 
 import android.app.Activity;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
 
+import com.example.trusspromiami.adapters.SliderAdapter;
+import com.example.trusspromiami.models.SliderModel;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.tabs.TabLayout;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    private ViewPager bannerSliderViewPager;
+    TabLayout indicator;
+    private Timer timer;
+    final private long DELAY_TIME = 2000;
+    final private long PERIOD_TIME = 2000;
+    private int currentPage = 2;
+    List<SliderModel> sliderModelList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +44,61 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
 
         // set custom toolbar
-        Toolbar toolbar =  findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // slider images
+
+        bannerSliderViewPager = findViewById(R.id.banner_slider_view_Pager);
+        indicator = findViewById(R.id.indicator);
+
+        sliderModelList = new ArrayList<SliderModel>();
+
+        sliderModelList.add(new SliderModel(R.drawable.one));
+        sliderModelList.add(new SliderModel(R.drawable.two));
+        sliderModelList.add(new SliderModel(R.drawable.three));
+        sliderModelList.add(new SliderModel(R.drawable.four));
+
+
+
+        SliderAdapter sliderAdapter = new SliderAdapter(sliderModelList);
+        bannerSliderViewPager.setAdapter(sliderAdapter);
+        bannerSliderViewPager.setClipToPadding(false);
+        bannerSliderViewPager.setPageMargin(20);
+        indicator.setupWithViewPager(bannerSliderViewPager, true);
+        ViewPager.OnPageChangeListener onPageChangeListener = new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                currentPage = position;
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                if (state == ViewPager.SCROLL_STATE_IDLE) {
+                    pageLope();
+                }
+
+            }
+        };
+        bannerSliderViewPager.addOnPageChangeListener(onPageChangeListener);
+        startBannerSlideShow();
+
+        bannerSliderViewPager.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                pageLope();
+                stopBannerSlideShow();
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    stopBannerSlideShow();
+                }
+                return false;
+            }
+        });
+
     }
 
 
@@ -78,5 +153,40 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+
+    private void pageLope() {
+        if (currentPage == sliderModelList.size()) {
+            bannerSliderViewPager.setCurrentItem(currentPage++, false);
+        }
+
+    }
+
+    private void startBannerSlideShow() {
+        final Handler handler = new Handler();
+        final Runnable update = new Runnable() {
+            @Override
+            public void run() {
+                if (currentPage >= sliderModelList.size()) {
+                    currentPage = 0;
+                }
+                bannerSliderViewPager.setCurrentItem(currentPage++, true);
+
+            }
+        };
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(update);
+            }
+        }, DELAY_TIME, PERIOD_TIME);
+
+
+    }
+
+    private void stopBannerSlideShow() {
+        timer.cancel();
     }
 }
--- app/src/main/java/com/example/trusspromiami/adapters/SliderAdapter.java
+++ app/src/main/java/com/example/trusspromiami/adapters/SliderAdapter.java
@@ -1,4 +1,48 @@
 package com.example.trusspromiami.adapters;
 
-public class SliderAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.example.trusspromiami.R;
+import com.example.trusspromiami.models.SliderModel;
+
+import java.util.List;
+
+public class SliderAdapter extends PagerAdapter {
+    private List<SliderModel> sliderModelList;
+
+    public SliderAdapter(List<SliderModel> sliderModelList) {
+        this.sliderModelList = sliderModelList;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        View view = LayoutInflater.from(container.getContext()).inflate(R.layout.item_slider, container, false);
+        ImageView banner = view.findViewById(R.id.banner_slide);
+        banner.setImageResource(sliderModelList.get(position).getBanner());
+        container.addView(view,0);
+        return view;
+    }
+
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View)object);
+    }
+
+    @Override
+    public int getCount() {
+        return sliderModelList.size();
+    }
 }
